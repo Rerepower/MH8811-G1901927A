0f6e8ff5f6699557eb63de7789c402b64d4825fe
Rerepower <bsl0216cn@gmail.com>
2019-09-26 16:32:47 +0800

    Done

---
+++ 02/02_C to F Converter.py
@@ -0,0 +1,6 @@
+# Ask user to enter a Celsius temperature
+Celsius=float(input("Please enter a temperature in Celsius: "))
+# Convert the celsius temperature into fahrenheit
+Fahrenheit=Celsius*1.8+32
+# Print the result
+print(str(Celsius) + ' Celsius in Fahrenheit is ' + str(Fahrenheit))
--- 02/02_Hello world.py
+++ 02/02_Hello world.py
@@ -1,2 +1,4 @@
-username=input()
+# Ask users to enter their name
+username=input("Please input your name: ")
+# Print the result
 print('Hello, ',username,' !')
