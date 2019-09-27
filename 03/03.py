26dbd1d4f4b6a6bcaae8431eba5e497f652b62e3
Rerepower <bsl0216cn@gmail.com>
2019-09-27 22:14:32 +0800

    03

---
+++ 03/03.py
@@ -0,0 +1,20 @@
+# exit option 
+#back to selection
+
+# -------------------------- 01.py
+def func1()
+print("Hello, world!")
+#--------------------------
+
+#---------------------------02.C to F
+
+Celsius=float(input("Please enter a temperature in Celsius: "))
+Fahrenheit=Celsius*1.8+32
+print(str(Celsius) + ' Celsius in Fahrenheit is ' + str(Fahrenheit))
+
+#-------------------------
+
+#-----------------------02_Hello world
+
+username=input("Please input your name: ")
+print('Hello, ',username,' !')
