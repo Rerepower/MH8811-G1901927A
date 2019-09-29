32aac469b34085788aceb5049dfc6f3541b8c5e5
Rerepower <bsl0216cn@gmail.com>
2019-09-29 16:23:25 +0800

    Python-style variable and function name

--- 03/03.py
+++ 03/03.py
@@ -1,40 +1,40 @@
 # 3rd Homework
-# Design a program with 3 sub-program given users to run "Hello, world" -
+# Design a program with 3 sub-program given users choices to run "Hello, world" -
 # - Personlized hello world and Celsius to Fahrenheit convert program with loop and exit function
 # Define Hello world Function
-def funcHelloWorld():
+def func_hello_world():
     print("Hello, world!")
 #----------------------------------------------------#
 # Define User's Hello world function
-def funcUserHelloWorld():
+def func_user_hello_world():
     username = input("Please input your name: ")
     print('Hello, ',username,' !')
 #----------------------------------------------------#
 # Define Celsius to Fahrenheit convert function
-def funcTempConvert():
+def func_temp_convert():
     # Ask user to put number in case he or she enters a character
     while True:
         try:
             str_num = input('Please enter a temperature in Celsius: ')
-            Celsius = float(str_num)
+            celsius = float(str_num)
             break
         except:
             print('Incorrect input, please enter a NUMBER in Celsius!')
-    Fahrenheit = Celsius*1.8+32
-    print(str(Celsius) + ' Celsius in Fahrenheit is ' + str(Fahrenheit))
+    fahrenheit = celsius * 1.8 +  32
+    print(str(celsius) + ' Celsius in Fahrenheit is ' + str(fahrenheit))
 #----------------------------------------------------#
 # Sub-program loop
 while True:
     print('Choose the program you would like to run: \n Press 1 for printing "Hello, World!" \n Press 2 for Personalized Hello World \n Press 3 for Celsius to Fahrenheit Converter \n Press other keys to exit')
     x = input()
     if x == str(1):
-        funcHelloWorld()
+        func_hello_world()
         continue
     if x == str(2):
-        funcUserHelloWorld()
+        func_user_hello_world()
         continue
     if x == str(3):
-        funcTempConvert()
+        func_temp_convert()
         continue
     else:
         break
